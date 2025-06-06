9cf45dcafc7fe601b403530ce144e869b5531358
juaniwk3 <86119976+JuanIWK3@users.noreply.github.com>
2025-06-06 13:13:28 -0300

    init

---
+++ auth/src/main/java/dyhb/cv/auth/Controller.java
@@ -0,0 +1,12 @@
+package dyhb.cv.auth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String hello() {
+        return String.format("Hello");
+    }
+}
---
+++ auth/src/test/java/dyhb/cv/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package dyhb.cv.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
