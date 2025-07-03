2fc5c4aee482c5970eaa5f4396b351a18e17ed65
juaniwk3 <86119976+JuanIWK3@users.noreply.github.com>
2025-07-02 23:36:20 -0300

    feat: config and discovery

--- auth/src/main/java/dyhb/cv/auth/exceptions/FormatResponse.java
+++ auth/src/main/java/dyhb/cv/auth/exceptions/FormatResponse.java
@@ -1,23 +1,31 @@
 package dyhb.cv.auth.exceptions;
 
-import java.util.Date;
-
-import org.springframework.http.HttpStatus;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
-
+import java.util.Date;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 
 @Data
 public class FormatResponse {
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy HH:mm:SSSZ", timezone ="GMT-3")
+
+    @JsonFormat(
+        shape = JsonFormat.Shape.STRING,
+        pattern = "MM-dd-yyyy HH:mm:SSSZ",
+        timezone = "GMT-3"
+    )
     private Date timeStamp;
+
     private int httpStatusCode;
     private HttpStatus httpStatus;
     private String reason;
     private String message;
 
-    public FormatResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+    public FormatResponse(
+        int httpStatusCode,
+        HttpStatus httpStatus,
+        String reason,
+        String message
+    ) {
         this.timeStamp = new Date();
         this.httpStatusCode = httpStatusCode;
         this.httpStatus = httpStatus;
