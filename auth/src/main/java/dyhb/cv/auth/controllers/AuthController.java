ac1083c6cbe64a30fcb86bd1577e35a5193af165
juaniwk3 <86119976+JuanIWK3@users.noreply.github.com>
2025-07-02 21:56:22 -0300

    config folder and roadmap

--- auth/src/main/java/dyhb/cv/auth/controllers/AuthController.java
+++ auth/src/main/java/dyhb/cv/auth/controllers/AuthController.java
@@ -5,33 +5,42 @@ import dyhb.cv.auth.dto.LoginRequest;
 import dyhb.cv.auth.dto.LoginResponse;
 import dyhb.cv.auth.exceptions.EmailExistsException;
 import dyhb.cv.auth.services.AuthService;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @AllArgsConstructor
 public class AuthController {
+
+    @Autowired
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<Map<String, String>> createUser(@RequestBody CreateUserDto dto) throws EmailExistsException {
+    public ResponseEntity<Map<String, String>> createUser(
+        @RequestBody CreateUserDto dto
+    ) throws EmailExistsException {
         var user = authService.createUser(dto);
 
         Map<String, String> responseBody = new HashMap<>();
-        responseBody.put("message", "Usuário com email " + user.getEmail() + " criado com sucesso.");
+        responseBody.put(
+            "message",
+            "Usuário com email " + user.getEmail() + " criado com sucesso."
+        );
 
         return ResponseEntity.status(HttpStatus.CREATED).body(responseBody);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<LoginResponse> login(
+        @RequestBody LoginRequest loginRequest
+    ) {
         var loginResponse = authService.login(loginRequest);
         return ResponseEntity.ok(loginResponse);
     }
