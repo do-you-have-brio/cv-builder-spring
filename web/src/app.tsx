f5eb3ff2fb42095a7b705be0ecd032452cc6297d
Isaac Santiago <eoisaacc@gmail.com>
2025-07-02 22:14:34 -0300

    chore: configure tailwind and shadcn-ui

---
+++ web/src/app.tsx
@@ -0,0 +1,3 @@
+export const App = () => {
+  return <h1 className="bg-orange-400">Hello, world!</h1>;
+};
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { App } from "@/app.tsx";
+import "@/styles/globals.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
