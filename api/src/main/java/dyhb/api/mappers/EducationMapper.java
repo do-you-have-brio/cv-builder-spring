1f4837b25d5bc6b28be84020e96c2fb7dfa87713
juaniwk3 <86119976+JuanIWK3@users.noreply.github.com>
2025-07-10 17:09:44 -0300

    Format education mapper code style

--- api/src/main/java/dyhb/api/mappers/EducationMapper.java
+++ api/src/main/java/dyhb/api/mappers/EducationMapper.java
@@ -2,24 +2,34 @@ package dyhb.api.mappers;
 
 import dyhb.api.database.models.EducationModel;
 import dyhb.api.dto.EducationUpsertDto;
-import org.mapstruct.*;
-import org.mapstruct.factory.Mappers;
 import java.util.*;
 import java.util.stream.Collectors;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(
     nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
-    unmappedTargetPolicy = ReportingPolicy.IGNORE)
+    unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface EducationMapper {
-  EducationMapper INSTANCE = Mappers.getMapper(EducationMapper.class);
+    EducationMapper INSTANCE = Mappers.getMapper(EducationMapper.class);
 
-  EducationModel fromCreateDtoToModel(EducationUpsertDto dto, UUID userId);
+    EducationModel fromCreateDtoToModel(EducationUpsertDto dto, UUID userId);
 
-  default List<EducationModel> fromCreateDtosToModels(List<EducationUpsertDto> dtos, UUID userId) {
-    return dtos == null
-        ? List.of()
-        : dtos.stream().map(dto -> fromCreateDtoToModel(dto, userId)).collect(Collectors.toList());
-  }
+    default List<EducationModel> fromCreateDtosToModels(
+        List<EducationUpsertDto> dtos,
+        UUID userId
+    ) {
+        return dtos == null
+            ? List.of()
+            : dtos
+                .stream()
+                .map(dto -> fromCreateDtoToModel(dto, userId))
+                .collect(Collectors.toList());
+    }
 
-  EducationModel updateModelFromDto(EducationUpsertDto dto, @MappingTarget EducationModel model);
+    EducationModel updateModelFromDto(
+        EducationUpsertDto dto,
+        @MappingTarget EducationModel model
+    );
 }
